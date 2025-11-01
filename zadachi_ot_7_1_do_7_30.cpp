4d7bf6dbfbd11766fae68b9218b7aab7c99849c8
chinchopa3000 <cmaccaunt@gmail.com>
2025-11-01 19:02:11 +0300

    Задачи 7.1-7.30

---
+++ zadachi_ot_7_1_do_7_30.cpp
@@ -0,0 +1,368 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    // 7.1
+    printf("7.1:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.2
+    printf("7.2:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.3
+    printf("7.3:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.4
+    printf("7.4:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.5
+    printf("7.5:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.6
+    printf("7.6:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.7
+    printf("7.7:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.8
+    printf("7.8:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.9
+    printf("7.9:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.10
+    printf("7.10:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.11
+    printf("7.11:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%d ", i + j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.12
+    printf("7.12:\n");
+    for (i = 1; i <= 9; i++)
+    {
+        for (j = 1; j <= 9; j++)
+        {
+            printf("%d*%d=%d ", i, j, i * j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.13
+    printf("7.13:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%d ", i * j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.14
+    printf("7.14:\n");
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%d ", i + j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.15
+    printf("7.15:\n");
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%d ", i - j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.16
+    printf("7.16:\n");
+    for (i = 10; i <= 20; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.17
+    printf("7.17:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 10; j <= 20; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.18
+    printf("7.18:\n");
+    for (i = 1; i <= 8; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("# ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.19
+    printf("7.19:\n");
+    for (i = 8; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.20
+    printf("7.20:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("+ ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.21
+    printf("7.21:\n");
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("- ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.22
+    printf("7.22:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.23
+    printf("7.23:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.24
+    printf("7.24:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.25
+    printf("7.25:\n");
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.26
+    printf("7.26:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i * 10);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.27
+    printf("7.27:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j * 5);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.28
+    printf("7.28:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i + j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.29
+    printf("7.29:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i - j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 7.30
+    printf("7.30:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i * i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return 0;
+}
